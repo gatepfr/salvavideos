1dabe6990d49cd384ff7264494dda856deca7509
gatepfr <paulofab@gmail.com>
2026-05-31 16:02:38 -0300

    fix: add vitest/globals types, fix exclude globs, remove redundant @types/ffmpeg-static

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     globals: true,
-    exclude: ['node_modules', '.next'],
+    exclude: ['**/node_modules/**', '**/.next/**'],
   },
   resolve: {
     alias: {
